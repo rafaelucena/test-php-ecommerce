bb71359fb5b919d435a9797bd0a96e1b7852131c
rboszko <r.boszko@b4b.net.pl>
2020-04-08 14:06:53 +0200

    "019 - add product getter for 'id'"

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -13,6 +13,12 @@ class Product
     public function setId($id)
     {
         $this->id = $id;
+        return $this;
+    }
+
+    public function getId()
+    {
+        return $this->id;
     }
 
     /**
