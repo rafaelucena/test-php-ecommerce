7470611204c9cf2269aa81f59f70c4f99178c7db
rboszko <r.boszko@b4b.net.pl>
2020-04-08 13:34:05 +0200

    "003 - add unitprice property, getter and setter"

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,4 +4,16 @@ namespace Recruitment\Entity;
 
 class Product
 {
+    private $unitPrice;
+
+    public function setUnitPrice($price)
+    {
+        $this->unitPrice = $price;
+        return $this;
+    }
+
+    public function getUnitPrice()
+    {
+        return $this->unitPrice;
+    }
 }
