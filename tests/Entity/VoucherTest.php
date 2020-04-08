2c282b924cb8785af45b24d49bdbbd7290d317b0
rboszko <r.boszko@b4b.net.pl>
2020-04-08 23:48:10 +0200

    "094 - add new valid discount test for vouchertest"

--- tests/Entity/VoucherTest.php
+++ tests/Entity/VoucherTest.php
@@ -11,6 +11,23 @@ class VoucherTest extends TestCase
 {
     /**
      * @test
+     */
+    public function isDiscountValid()
+    {
+        $voucher = new Voucher();
+
+        $voucher->setCode('A8C6');
+        $this->assertEquals(20, $voucher->getDiscount());
+
+        $voucher->setCode('A8C4');
+        $this->assertEquals(15, $voucher->getDiscount());
+
+        $voucher->setCode('A8C2');
+        $this->assertEquals(10, $voucher->getDiscount());
+    }
+
+    /**
+     * @test
      * @expectedException \Recruitment\Entity\Exception\InvalidVoucherCodeException
      */
     public function itThrowsExceptionInvalidVoucherCode()
