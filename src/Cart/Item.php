e9f7f5d39838a0a1693092be5751ffd9d76ad01b
rboszko <r.boszko@b4b.net.pl>
2020-04-08 13:47:44 +0200

    "009 - add item setters"

--- src/Cart/Item.php
+++ src/Cart/Item.php
@@ -10,7 +10,19 @@ class Item
 
     public function __construct($product, $quantity)
     {
+        $this->setProduct($product);
+        $this->setQuantity($quantity);
+    }
+
+    private function setProduct($product)
+    {
         $this->product = $product;
+        return $this;
+    }
+
+    private function setQuantity($quantity)
+    {
         $this->quantity = $quantity;
+        return $this;
     }
 }
