af477b7cffab33d974ef66e5acf69e8412b81fb6
rboszko <r.boszko@b4b.net.pl>
2020-04-08 13:36:07 +0200

    "005 - add phpdoc to product entity"

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -4,14 +4,22 @@ namespace Recruitment\Entity;
 
 class Product
 {
+    /** @var integer */
     private $unitPrice;
 
+    /**
+     * @param integer $price
+     * @return self
+     */
     public function setUnitPrice(int $price): self
     {
         $this->unitPrice = $price;
         return $this;
     }
 
+    /**
+     * @return integer
+     */
     public function getUnitPrice(): int
     {
         return $this->unitPrice;
