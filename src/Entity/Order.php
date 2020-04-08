e9445d84a7b4789862815fa7cca87e2cb68974bd
rboszko <r.boszko@b4b.net.pl>
2020-04-08 16:30:38 +0200

    "058 - improve code into order object"

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -13,15 +13,28 @@ class Order
     public function __construct($id, $items)
     {
         $this->id = $id;
+        $this->setItemsAndTotalPriceData($items);
+    }
+
+    private function setItemsAndTotalPriceData($items)
+    {
+        $this->items = [];
+        $this->totalPrice = 0;
+
         /** @var Item $item */
         foreach ($items as $item) {
-            $this->items[] = [
-                'id' => $item->getProduct()->getId(),
-                'quantity' => $item->getQuantity(),
-                'total_price' => $item->getTotalPrice(),
-            ];
+            $this->items[] = $this->setItemData($item);
             $this->totalPrice += $item->getTotalPrice();
         }
+    }
+
+    private function setItemData($item)
+    {
+        return [
+            'id' => $item->getProduct()->getId(),
+            'quantity' => $item->getQuantity(),
+            'total_price' => $item->getTotalPrice(),
+        ];
     }
 
     public function getDataForView()
