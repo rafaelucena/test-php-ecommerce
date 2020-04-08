93f036bc6f7a9d2da8283f4af3bf22cad0a3a831
rboszko <r.boszko@b4b.net.pl>
2020-04-08 14:24:47 +0200

    "024 - add cart function for add product"

--- src/Cart/Cart.php
+++ src/Cart/Cart.php
@@ -2,7 +2,14 @@
 
 namespace Recruitment\Cart;
 
+use Recruitment\Cart\Item;
+
 class Cart
 {
     private $items;
+
+    public function addProduct($product, $quantity)
+    {
+        $this->items[$product->getId()] = new Item($product, $quantity);
+    }
 }
