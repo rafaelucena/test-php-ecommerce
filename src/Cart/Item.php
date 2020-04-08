ad711dc14bf05780446884b15f04ebba1efba84c
rboszko <r.boszko@b4b.net.pl>
2020-04-08 13:59:16 +0200

    "016 - add phpdoc to item object"

--- src/Cart/Item.php
+++ src/Cart/Item.php
@@ -22,34 +22,54 @@ class Item
         $this->setTotalPrice();
     }
 
+    /**
+     * @param Product $product
+     * @return self
+     */
     private function setProduct(Product $product): self
     {
         $this->product = $product;
         return $this;
     }
 
+    /**
+     * @return Product
+     */
     public function getProduct(): Product
     {
         return $this->product;
     }
 
+    /**
+     * @param integer $quantity
+     * @return self
+     */
     private function setQuantity(int $quantity): self
     {
         $this->quantity = $quantity;
         return $this;
     }
 
+    /**
+     * @return integer
+     */
     public function getQuantity(): int
     {
         return $this->quantity;
     }
 
+    /**
+     * @return self
+     */
     public function setTotalPrice(): self
     {
         $this->totalPrice = $this->product->getUnitPrice() * $this->quantity;
         return $this;
     }
 
+    /**
+     * @return float
+     */
     public function getTotalPrice(): float
     {
         return $this->totalPrice;
