89fd33c70216ef001acd22e6af5f1feb76b9d493
rboszko <r.boszko@b4b.net.pl>
2020-04-08 13:58:11 +0200

    "015 - add typesetting to item object"

--- src/Cart/Item.php
+++ src/Cart/Item.php
@@ -15,41 +15,42 @@ class Item
     /** @var float */
     private $totalPrice;
 
-    public function __construct($product, $quantity)
+    public function __construct(Product $product, int $quantity)
     {
         $this->setProduct($product);
         $this->setQuantity($quantity);
         $this->setTotalPrice();
     }
 
-    private function setProduct($product)
+    private function setProduct(Product $product): self
     {
         $this->product = $product;
         return $this;
     }
 
-    public function getProduct()
+    public function getProduct(): Product
     {
         return $this->product;
     }
 
-    private function setQuantity($quantity)
+    private function setQuantity(int $quantity): self
     {
         $this->quantity = $quantity;
         return $this;
     }
 
-    public function getQuantity()
+    public function getQuantity(): int
     {
         return $this->quantity;
     }
 
-    public function setTotalPrice()
+    public function setTotalPrice(): self
     {
         $this->totalPrice = $this->product->getUnitPrice() * $this->quantity;
+        return $this;
     }
 
-    public function getTotalPrice()
+    public function getTotalPrice(): float
     {
         return $this->totalPrice;
     }
